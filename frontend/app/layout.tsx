37dcdca379dfc6f16462a1513c00dda4bcd8ca26
Manjurul Hoque Rumi <rumi@impelitsolutions.com>
2025-08-24 01:26:42 +0600

    Update Toaster component in layout to include closeButton prop for improved user interaction

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
                         disableTransitionOnChange
                     >
                         {children}
-                        <Toaster />
+                        <Toaster closeButton />
                     </ThemeProvider>
                 </QueryProvider>
             </body>
